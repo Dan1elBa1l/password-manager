2f8f1029559945f3c1c8cfc4842a76a01028f1d1
Daniel Bail <Daniel.Bail@Student.HTW-Berlin.de>
2023-12-21 18:04:26 +0000

    Changed Config

--- src/main/java/htw/berlin/webtech/Password/Password.java
+++ src/main/java/htw/berlin/webtech/Password/Password.java
@@ -29,37 +29,44 @@ public class Password {
         this.password = password;
         this.description = description;
     }
-        public void setService(String service) {
-            this.service = service;
-        }
 
-        public String getService() {
-            return service;
-        }
-
-        public void setUsername(String username) {
-            this.username = username;
-        }
-
-        public String getUsername() {
-            return username;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public String getDescription() {
-            return description;
-        }
+    public Long getId() {
+        return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- src/main/java/htw/berlin/webtech/WebConfig.java
+++ src/main/java/htw/berlin/webtech/WebConfig.java
@@ -8,8 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173"); // Ersetzen Sie dies mit der tatsächlichen Herkunft Ihres Frontends
-    }
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:5173")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*");
+        }
 }
